c678f145e41712e4821495670e1e608fd0ae34d3
MainDen <50581460+MainDen@users.noreply.github.com>
2021-03-11 00:54:36 +0300

    Copyright changed.
    Changes:
    Added "MainDen" to copyright.

--- Client Socket Toolkit by MainDen/Properties/AssemblyInfo.cs
+++ Client Socket Toolkit by MainDen/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Client Socket Toolkit by MainDen")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © MainDen 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
